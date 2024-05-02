bbdc6327e9bb481197aab8e9d8f01007efe8221e
f4st3r <152923524+f4st3rxd@users.noreply.github.com>
2024-05-03 00:43:05 +0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,15 @@
+FastConfig = {}
+
+FastConfig.WhitelistCars = { -- cars that can be used by specified identifiers
+    "f4stbmwm5",
+    "f4stbmwm4",
+}
+
+FastConfig.NotifyText = "You don't know how to drive this vehicle" -- notification if someone other than the designated identifier drives the vehicle
+
+FastConfig.IdentifierType = "discord" -- steam or license or discord or license2
+
+FastConfig.Identifiers = { -- the identifiers specified in the FastConfig.IdentifierType setting can be used
+    "discord:724041053719494757",
+    "discord:"
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'f4st3r'
+description 'donate protection script'
+
+client_scripts {
+    'client/*.lua'
+}
+
+server_scripts {
+    'server/*.lua'
+}
+
+shared_scripts {
+    'config.lua'
+}
